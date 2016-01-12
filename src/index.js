4af25082fe65b349a908ddc11c2e286b38437caf
kkomaz <leeajl006@gmail.com>
2016-01-11 21:34:07 -0500

    Youtube Search API setup complete

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// npm install --save youtube-api-search
+const API_KEY = 'AIzaSyB90AZGcP9JX5mqW2b6F-T-crfz6OY2pdY';
+
 // Create a new component.  This component should produce some HTML
 
 const App = () => {
