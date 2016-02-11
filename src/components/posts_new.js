57c7911c2bcf57161268df05851a87d0ee7eccf0
kkomaz <leeajl006@gmail.com>
2016-02-11 09:06:23 -0500

    create basic form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
   render(){
     return (
-      <div>Create Form</div>
+      <form>
+        <h3>Create a New Post</h3>
+          <div className="form-group">
+            <label>Title</label>
+            <input type="text" className="form-control"/>
+            <label>Categories</label>
+            <input type="text" className="form-control"/>
+            <label>Content</label>
+            <textarea className="form-control"/>
+          </div>
+          <button type="submit" classNae="btn btn-primary">Submit</button> 
+      </form>
     );
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNew'
+  fields: ['title', 'categories', 'content']
+})(PostsNew);
+
+// Name of form does not need to match
+
+// user types something in...record it on application state
+
+// state === {
+//   form: {
+//     PostsNewForm: {
+//       title: '....',
+//       categories: '....',
+//       content: '.....'
+//     }
+//   }
+// }
