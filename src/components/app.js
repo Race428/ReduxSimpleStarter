f0bdff989a86976db919554b89f6f4f98124df68
kkomaz <leeajl006@gmail.com>
2016-02-14 19:50:13 -0500

    Passing control to redux form via helpers

--- src/components/app.js
+++ src/components/app.js
@@ -6,7 +6,6 @@ export default class App extends Component {
     return (
       <div>
         {this.props.children}
-        Hello world
       </div>
     );
   }
--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -3,16 +3,20 @@ import { reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
   render(){
+    const { fields: { title, categories, content }, handleSubmit } = this.props;
+    // const handleSubmit = this.props.handleSubmit;
+    // const title = this.props.fields.title
+
     return (
-      <form>
+      <form onSubmit={handleSubmit}>
         <h3>Create a New Post</h3>
           <div className="form-group">
             <label>Title</label>
-            <input type="text" className="form-control"/>
+            <input type="text" className="form-control" {...title} />
             <label>Categories</label>
-            <input type="text" className="form-control"/>
+            <input type="text" className="form-control" {...categories} />
             <label>Content</label>
-            <textarea className="form-control"/>
+            <textarea className="form-control" {...content} />
           </div>
           <button type="submit" classNae="btn btn-primary">Submit</button> 
       </form>
@@ -21,7 +25,7 @@ class PostsNew extends Component {
 }
 
 export default reduxForm({
-  form: 'PostsNew'
+  form: 'PostsNew',
   fields: ['title', 'categories', 'content']
 })(PostsNew);
 
