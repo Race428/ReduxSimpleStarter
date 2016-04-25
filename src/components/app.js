1173cd29e2c53a58ed33bc6e8d55b2f28e3019af
kkomaz <leeajl006@gmail.com>
2016-04-25 14:01:43 -0400

    React Responsive Tutorial

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import { Component } from 'react';
+import DesktopBreakpoint from './responsive_utilities/desktop_breakpoint';
+import TabletBreakpoint from './responsive_utilities/tablet_breakpoint';
+import PhoneBreakpoint from './responsive_utilities/phone_breakpoint';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <h2>My Breakpoints!</h2>
+        
+        <DesktopBreakpoint>
+          <h3>DesktopBreakpoint!</h3>
+        </DesktopBreakpoint>
+
+        <TabletBreakpoint>
+          <h3>TabletBreakpoint!</h3>
+        </TabletBreakpoint>
+
+        <PhoneBreakpoint>
+          <h3>PhoneBreakpoint</h3>
+        </PhoneBreakpoint>
+      </div>
     );
   }
 }
